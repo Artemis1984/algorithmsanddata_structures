22f943f1c54fcc4f58c61f77f78c9c95490e6f15
paruyr123 <paruyr-shahbazyan@yandex.ru>
2019-06-20 01:48:13 +0300

    ДЗ по уроку 5

--- Lesson5/Task_2.py
+++ Lesson5/Task_2.py
@@ -9,43 +9,40 @@ from collections import deque
 
 hex_numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f']
 
-number2 = deque(input("Введите первое шестнадцатеричное число: "))
-number1 = deque(input("Введите второе шестнадцатеричное число: "))
 memo_number = 0
-
-
-if len(number1) > len(number2):
-    number2.extendleft('0' * (len(number1) - len(number2)))
-elif len(number2) > len(number1):
-    number1.extendleft('0' * (len(number2) - len(number1)))
-
-
 result = deque()
 
-for i in range(len(number1) - 1, -1, -1):
+while True:
 
-    if number1[i] in hex_numbers and number2[i] in hex_numbers:
+    number2 = deque(input("Введите первое шестнадцатеричное число: "))
+    number1 = deque(input("Введите второе шестнадцатеричное число: "))
 
-        repo = hex_numbers.index(number1[i]) + hex_numbers.index(number2[i])
+    if len(number1) > len(number2):
+        number2.extendleft('0' * (len(number1) - len(number2)))
+    elif len(number2) > len(number1):
+        number1.extendleft('0' * (len(number2) - len(number1)))
 
-        if repo > len(hex_numbers) - 1:
-            if i == len(number1) - 1:
+    for i in range(len(number1) - 1, -1, -1):
+
+        if number1[i] in hex_numbers and number2[i] in hex_numbers:
+
+            repo = hex_numbers.index(number1[i]) + hex_numbers.index(number2[i])
+
+            if repo > len(hex_numbers) - 1:
+
+                result.appendleft(hex_numbers[repo - len(hex_numbers) + memo_number])
+                memo_number = 1
+
+            else:
+                if repo + memo_number > len(hex_numbers) - 1:
+                    result.appendleft('0')
+                else:
+                    result.appendleft(hex_numbers[repo + memo_number])
                 memo_number = 0
 
-            result.appendleft(hex_numbers[repo - len(hex_numbers) + memo_number])
-            memo_number = 1
+    if memo_number == 1:
+        result.appendleft(memo_number)
 
-        else:
-            if repo + memo_number > len(hex_numbers) - 1:
-                result.appendleft('0')
-            else:
-                result.appendleft(hex_numbers[repo + memo_number])
-            memo_number = 0
-
-if memo_number == 1:
-    result.appendleft(memo_number)
-
-
-print(*result)
+    print(*result)
 
 
