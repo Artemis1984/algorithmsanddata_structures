d91cfa6374e3f220977309b601a3c23f0a9aa014
paruyr123 <paruyr-shahbazyan@yandex.ru>
2019-06-19 14:30:51 +0300

    ДЗ по уроку 5

--- Lesson5/Task_1.py
+++ Lesson5/Task_1.py
@@ -36,16 +36,26 @@ for i in all_companies:
 print(f"Средняя прибыль за год всех компаний составляет: {sum_/len(all_companies)}$")
 
 
+
 for i in all_companies:
+
     each_sum = 0
+
     for j in range(1, len(i._fields)):
+
         each_sum += int(i[j])
 
+
     if each_sum < sum_/len(all_companies):
+
         print(f"У компании {i.name} прибыль меньше среднего: {each_sum}$")
+
     elif each_sum > sum_/len(all_companies):
+
         print(f"У компании {i.name} прибыль больше среднего: {each_sum}$")
+
     else:
+
         print(f"У компании {i.name} прибыль равна среднему: {each_sum}$")
 
 
